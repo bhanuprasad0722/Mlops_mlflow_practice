81e82e85a55c8d49752863dc8d810b497a1b3b92
bhanuprasad0722 <bhanuprasadchellapuram>
2025-09-06 11:52:54 +0530

    tracked params and metrics using mlflow

---
+++ src/mlflow_practice1.py
@@ -0,0 +1,34 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt 
+
+
+#loading the data
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# splitting the data
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=42,test_size=0.2)
+
+# parameter
+max_depth = 7
+n_estimators = 10
+
+# starting an mlflow run
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators)
+    rf.fit(X_train,y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test,y_pred)
+# logging metrics and parameter
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    mlflow.log_param('n_estimators',n_estimators)
+
+print("Successfully logged matrics and params")
+
